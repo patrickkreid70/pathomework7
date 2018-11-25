60b71dbc98d766174471b7c0a87c2cd281c38faf
Matthew Davis <maathewdavis@gmail.com>
2018-11-25 16:25:26 -0500

    we got some spaghetti in here let me tell you boy

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: ["airbnb-base", "prettier"],
   rules: {
-    "no-console": 0
-  }
+    "no-console": 0,
+    "no-continue": 0,
+    "no-unused-vars": 0
+  },
+  env: { es6: true, browser: true }
 };
---
+++ public/sim.js
@@ -0,0 +1,79 @@
+const emojis = [
+  ..."🌵🎄🌲🌳🌴🌱🌿☘🍀🎋🍃🍄🐢🐤🐣🦆🐖🍄🌾💐🌷🌹🥀🌺🐪🐶🐱🐭🐹🐰🦅🦀🦇🐸🐉🌞🌈🌻"
+];
+const rows = {};
+let simpsonsScore;
+const generateRow = chars => {
+  const randomChars = chars.sort(() => 0.5 - Math.random());
+  return randomChars
+    .slice(0, 8)
+    .reduce((string, emoji) => {
+      if (Math.random() > 0.6) {
+        return string.concat("\u3000");
+      }
+      return string.concat(emoji);
+    }, "")
+    .concat("\n");
+};
+const appendNode = (parentNode, childTag, innerHTML, number) => {
+  const node = document.createElement(childTag);
+  node.innerHTML = innerHTML;
+  node.classList.add("row");
+  parentNode.appendChild(node);
+  if (rows[number] && rows[number].pinned) {
+    node.classList.add("pinned");
+  }
+  node.addEventListener("click", function onClickRow(e) {
+    this.classList.toggle("pinned");
+    rows[number].pinned = !rows[number].pinned;
+    console.log(rows);
+  });
+};
+
+const simpsonsIndex = forest =>
+  1 -
+  Object.entries(
+    [...forest.join("")].reduce(
+      (counts, emoji) => ({ ...counts, [emoji]: (counts[emoji] || 0) + 1 }),
+      {}
+    )
+  )
+    .reduce(
+      ([top, bottom], [_species, count]) => [
+        top + count * (count - 1),
+        bottom + count
+      ],
+      [0, 0]
+    )
+    .reduce((sumLilN, bigN) => sumLilN / (bigN * (bigN - 1)));
+const generateRandom = () => {
+  const sim = document.querySelector("div#sim");
+  sim.innerHTML = "";
+  const arrayofRows = [];
+  for (let i = 0; i < 8; i += 1) {
+    const row = generateRow(emojis);
+    if (rows[i] && rows[i].pinned) {
+      appendNode(sim, "P", rows[i].string, i);
+      arrayofRows.push(rows[i].string);
+      continue;
+    }
+    arrayofRows.push(row);
+    appendNode(sim, "P", row, i);
+    rows[i] = { string: row };
+  }
+  simpsonsScore = simpsonsIndex(arrayofRows);
+  document.querySelector("h2").innerText = `SIMPSONS SCORE: ${simpsonsScore}`;
+  console.log(rows);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("it's loaded baby");
+  const generateButton = document.querySelector("button");
+  const intro = document.querySelector("div#intro");
+  const h2dumb = document.createElement("H2");
+  h2dumb.innerText = simpsonsScore ? `SIMPSONS SCORE: ${simpsonsScore}` : "";
+  intro.appendChild(h2dumb);
+  generateButton.addEventListener("click", () => {
+    generateRandom();
+  });
+});
