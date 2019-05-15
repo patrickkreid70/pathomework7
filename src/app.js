348bacfb75132b67dd902add4b54d178eecf4524
Patrick <patrick7@bu.edu>
2019-05-15 15:27:00 -0400

    r

--- src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-
-app.use(express.static(path.join(__dirname, "../public")));
-app.use(express.urlencoded({ extended: false }));
-
-app.get("/index.html");
-app.listen(3000, () => console.log("Listening on 3000"));
