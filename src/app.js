01128c5a21c10a8fbb4b3d6abde2aec2e5e4b8e2
Patrick <patrick7@bu.edu>
2019-05-15 15:24:21 -0400

    se

--- src/app.js
+++ src/app.js
@@ -4,4 +4,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/index.html");
 app.listen(3000, () => console.log("Listening on 3000"));
